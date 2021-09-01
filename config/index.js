1ec61197e6c6d28acb852567a5e939dec9555e9c
Nanna <evg2287@gmail.com>
2021-09-01 15:34:31 +0300

    fix: fixed api port into config

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 export default {
   host: process.env.HOST || '127.0.0.1',
-  port: process.env.PORT || 3051,
+  port: process.env.PORT || 3052,
   locationLqAPIUrl: process.env.LOCATIONLQ_API_URL || 'https://eu1.locationiq.com/v1/search.php?key=*KEY&q="*ADRESS"&format=json',
   locationLqAPIKey: process.env.LOCATIONLQ_API_KEY || 'pk.159a6a2e31219ffa77b5562a34bc3b5c',
 };
